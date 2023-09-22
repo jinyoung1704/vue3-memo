95a813aafb1c41500fc1f0860656f5b3a36f5768
jinyoung1704 <jinyoung1704@naver.com>
2023-09-22 16:16:13 +0900

    backend 노트지 추가

--- backend/api-server.js
+++ backend/api-server.js
@@ -5,10 +5,13 @@ const port = 3000
 const bodyParser = require("body-parser"); //req.body를 사용하기 위해 
 
 const memos = [];
+const newPost = [];
+
 
 app.use(bodyParser.json());
 
 // 첫번째로 화면에 들어오면 hello world 값을 보낸다
+//memo
 app.get('/api/memos', (req, res) => {
   res.send(memos)
 })
@@ -23,6 +26,19 @@ app.put("/api/memos/:idx",(req,res)=>{
   //console.log(req.params.idx);
   memos[req.params.idx] = req.body.content;
   res.send(memos)
+})
+
+//todo
+app.get('/api/addPost', (req, res) => {
+  res.send(newPost)
+})
+
+app.post("/api/addPost",(req,res)=>{
+  console.log('======post========');
+  console.log('req.body',req.body.newPost);
+  newPost.push(req.body.newPost);
+
+  res.send(newPost)
 })
 
 app.listen(port, () => {
